eb6f53d6a972185f371d3e62ed3a181a7e528dd0
MyNewKey <1608581096@qq.com>
2020-03-09 01:25:17 +0800

    init repo

---
+++ src/main/java/life/majiang/community/CommunityApplication.java
@@ -0,0 +1,17 @@
+package life.majiang.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author chenzhichao
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
